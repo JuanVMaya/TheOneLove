1481e04565d11a0075643963271e6b5d192d4145
JuanVMaya <juanjovanegas21@gmail.com>
2024-02-13 23:53:27 -0500

    Add finalScreen pictures

--- src/components/FinalScreen.jsx
+++ src/components/FinalScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import house from "../assets/house.jpg";
+import kitties from "../assets/kitties.png";
 import useProgressStore from "../stores/progress";
 
 const FinalScreen = () => {
@@ -13,6 +14,7 @@ const FinalScreen = () => {
     <div className="flex flex-col gap-4 p-4 justify-center align-middle">
       <h1 className="font-bold text-2xl text-center">Happy Valentine's Day</h1>
       <p className="label-text-alt text-center">and birthday 🎂</p>
+      <img src={kitties} className="w-56 h-48 bg-cover bg-center self-cente" />
       <img
         src={house}
         className="w-56 h-48 bg-cover bg-center heart-shape self-center"
